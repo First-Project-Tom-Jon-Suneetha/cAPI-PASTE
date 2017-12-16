262f83dbc3359c4544ba97ff06ce7728ea0f35a1
thomas cote <tcote19@gmail.com>
2017-12-16 13:47:51 -0800

    New user auth to firebase

--- auth.js
+++ auth.js
@@ -4,104 +4,58 @@
     authDomain: "capi-paste-9e165.firebaseapp.com",
     databaseURL: "https://capi-paste-9e165.firebaseio.com",
     projectId: "capi-paste-9e165",
-    storageBucket: "",
+    storageBucket: "capi-paste-9e165.appspot.com",
     messagingSenderId: "846503561187"
   };
   firebase.initializeApp(config);
 
-var textEmail = document.getElementById("#user-email");
-var textPassword = document.getElementById("#user-password");
-var loginBtn = document.getElementById("#button-login");
+var existingEmail = $("#email-text");
+var existingPassword = $("#password-text");
+var loginBtn = $("#submit-up");
 
-$(loginBtn).on("click", function(e){
-	textEmail.val().trim();
-	textPassword.val().trim();
-})
+var signupName = $("#user-name");
+var signupEmail = $("#signup-email");
+var signupPass = $("#signup-password");
+var signupBtn = $("#signup-submit");
 
 
+// Existing user login function
+$(loginBtn).on("click", function(){
+  event.preventDefault();
 
-  https://capi-paste-9e165.firebaseapp.com/__/auth/handler
+	var existingUserEmail = existingEmail.val().trim();
+	var existingUserPassword = existingPassword.val().trim();
 
-// USER CREATES NEW ACCOUT
-  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // ...
-});
-email.html
+  console.log(existingUserEmail);
+  console.log(existingUserPassword);  
 
-// USER SIGNS IN WITH EXISTING ACCOUT
-firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // ...
-});
-email.html
-
-// GLOBAL OBSERVER FOR GETTING DETAILS ABOUT THE USER THROUGHOUT THE SITE
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in.
-    var displayName = user.displayName;
-    var email = user.email;
-    var emailVerified = user.emailVerified;
-    var photoURL = user.photoURL;
-    var isAnonymous = user.isAnonymous;
-    var uid = user.uid;
-    var providerData = user.providerData;
-    // ...
-  } else {
-    // User is signed out.
-    // ...
-  }
-});
-email.html
-
-var provider = new firebase.auth.GithubAuthProvider();
-
-firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
-  // ...
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
 });
 
-// REDIRECT SIGN IN
-firebase.auth().signInWithRedirect(provider);
+// New user signup function
+$(signupBtn).on("click", function(){
+  event.preventDefault();
 
-// GET REDIRECT RESULTS
-firebase.auth().getRedirectResult().then(function(result) {
-  if (result.credential) {
-    // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-    var token = result.credential.accessToken;
-    // ...
-  }
-  // The signed-in user info.
-  var user = result.user;
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
+  var signupUsersName = signupName.val().trim();
+  var signupUserEmail = signupEmail.val().trim();
+  var signupUserPassword = signupPass.val().trim();
+
+  console.log(signupUsersName);
+  console.log(signupUserEmail);
+  console.log(signupUserPassword); 
+
+  var newUserObject = {
+    "name": signupUsersName,
+    "email": signupUserEmail,
+    "password": signupUserPassword
+  }; 
+
+  var database = firebase.database();
+
+  database.ref("accounts").push({
+    newUserObject
+  });
+
 });
-
-
 
 
 
